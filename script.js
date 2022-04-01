6e09af59e065945337e7bdb6853e0b681843c85f
Kenyansa <kenyansajustine@gmail.com>
2022-04-01 12:06:57 +0300

    add button for 'hire me'

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
-
+            $('.navbar').addClass("sticky");
         }else{
-            
+            $('.navbar').removeClass("sticky");
         }
 
 
