cec1eeda0514369f079440d2f79adac8317abd8b
Kenyansa <kenyansajustine@gmail.com>
2022-04-04 07:03:16 +0300

    introduce toggle function to the skills section

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function () {
+  $("#aws1").on("click", function () {
+    $("#aws").toggle();
+    $("#awsImage").toggle();
+  });
+
+  $("#huawei1").on("click", function () {
+    $("#huawei").toggle();
+    $("#huaweiImage").toggle();
+  });
+
+  $("#dataAnalytics1").on("click", function () {
+    $("#dataAnalytics").toggle();
+    $("#dataAnalyticsImage").toggle();
+  });
+});
